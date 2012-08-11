3201d82795262dec356f5f3dbedca95be818c1e1
Ryan <rocifier@gmail.com>
2012-08-12 09:59:49 +1200

    new fabric.js canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,26 @@
 
 $(function() { // document ready, resize container
 
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  
+  var canvas = new fabric.Canvas('canvas'),
+  	  ctx = (function(){
+		  return canvas.getContext 
+			? canvas.getContext('2d') 
+			: typeof G_vmlCanvasManager != 'undefined' 
+			  ? G_vmlCanvasManager.initElement(canvas).getContext('2d')
+			  : null;
+		})();
+	if (!ctx) return;
+	
+    var rect = new fabric.Rect({
+      top: 100,
+      left: 100,
+      width: 60,
+      height: 70,
+      fill: 'red'
+    });
+
+    canvas.add(rect);
+    
   var rc = 0;  // resize counter
   var oc = 0;  // orientiation counter
   var ios = navigator.userAgent.match(/(iPhone)|(iPod)/); // is iPhone
@@ -26,13 +43,13 @@ $(function() { // document ready, resize container
 	  
 	  var width = $("#container").width();
 	  var height = $("#container").height();
-	  
 	  cheight = height - 20; // subtract the fix height
 	  cwidth = width;
 	  
 	  // set canvas width and height
-	  $("#canvas").attr('width', cwidth);
-	  $("#canvas").attr('height', cheight)
+	  canvas.setWidth(cwidth);
+	  canvas.setHeight(cheight);
+	  canvas.calcOffset();
 
 	  // hides the WebKit url bar
 	  if (ios) {
@@ -40,16 +57,6 @@ $(function() { // document ready, resize container
 			  window.scrollTo(0, 1);
 		  }, 100);   
 	  }
-	  ctx.fillStyle = 'green';
-	  ctx.fillRect(0, 0, cwidth, cheight);
-	  ctx.fillStyle = 'black';
-	  ctx.fillRect(10, 10, cwidth - 20, cheight - 20)
-
-	  // write number of orientation changes and resize events
-	  ctx.fillStyle = 'white';
-	  ctx.textAlign = 'center';
-	  ctx.fillText('Orientiation changes: '+oc, cwidth/2, cheight/2);
-	  ctx.fillText('Resize events: '+rc, cwidth/2, cheight/2 + 10);
   }
 
   var resizeTimeout;
